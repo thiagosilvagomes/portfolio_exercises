b436d774b911d7df5a92a335daa506cb52d777da
Thiago da Silva Gomes <thiagosilvagomes@gmail.com>
2020-09-18 19:00:21 -0300

    exercicios bloco 5

--- bloco_5/5_1/script_class.js
+++ bloco_5/5_1/script_class.js
@@ -1,5 +1,5 @@
-let pilotosDeF1 = document.getElementsByClassName("piloto-f1-atual")[0].innerText = "Stroll"; //É um vetor
-for (let i=0; i<pilotosDeF1.length; i++)
+let pilotosDeF1 = document.getElementsByClassName("piloto-f1-atual"); //É um vetor
+for (let i=0; i<pilotosDeF1.length; i += 1)
 {
-   pilotosDeF1[i] = "Lewis Hamilton";
+   pilotosDeF1[i].innerText = "Lewis Hamilton";
 } 
--- bloco_5/5_2/script.js
+++ bloco_5/5_2/script.js
@@ -1,11 +1,19 @@
-let ingredientItems = {
-    `500g de feijão carioquinha cozido`,
-    `200g de toucinho`,
-    `1 concha de óleo`,
-    `1 cebola média picada`,
-    `4 dentes de alho`,
-    `5 ovos`,
-    `1 colher de sopa de sal com alho`,
-    `Cheiro verde a gosto`,
-    `200g de farinha de mandioca`
-};
+let ingredientItems = [
+    "500g de feijão carioquinha cozido",
+    "200g de toucinho",
+    "1 concha de óleo",
+    "1 cebola média picada",
+    "4 dentes de alho",
+    "5 ovos",
+    "1 colher de sopa de sal com alho",
+    "Cheiro verde a gosto",
+    "200g de farinha de mandioca"
+];
+
+let ingredientList = document.querySelector('.ingredients-list');
+for (let index=0; index < ingredientItems.length; index++)
+{
+    let ingredientListItem = document.createElement('li'); //Cria o tópico da lista
+    ingredientListItem.innerText = ingredientItems[index]; // Atribui o valor do vetor na posicao index ao elemento criado anteriormente
+    ingredientList.appendChild(ingredientListItem);
+}
