5487e5ae89284facb1d6179f4e19ad3dc59fd42b
Thiago da Silva Gomes <thiagosilvagomes@gmail.com>
2021-01-28 21:19:06 -0300

    Exercícios_Bloco_9_2

--- bloco_9/9_2/exercises/javascript-assincrono-e-promises_2/exercicio02.js
+++ bloco_9/9_2/exercises/javascript-assincrono-e-promises_2/exercicio02.js
@@ -3,12 +3,12 @@ const createArray = (() => {
         const arrayRandom = [];
         const length = 9;
         for (let cont = 0; cont <= length; cont++)
-            arrayRandom[cont] = Math.round(Math.random(5)*50);
-        const arrayAoQuadrado = arrayRandom.map((number) => {
+            arrayRandom[cont] = Math.round(Math.random()*50);
+            const arrayAoQuadrado = arrayRandom.map((number) => {
             return (number * number);
         });
         const somatorio = arrayAoQuadrado.reduce((result, number) => result + number);
-        if (somatorio <= 8000){
+        if (somatorio <= 8000) {
             resolve(somatorio);
         }
         else {
@@ -20,7 +20,6 @@ createArray()
     .then((soma) => {
         const divNumeros = [2,3,5,10];
         console.log(`Somatorio menor ou igual à 8000`);
-        console.log(soma);
         return arrayResultDiv = divNumeros.map((number) => soma / number);
     })
     .catch(() => console.log("É mais de oito mil! Essa promise deve estar quebrada!"));
