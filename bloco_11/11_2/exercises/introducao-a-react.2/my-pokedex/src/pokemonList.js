d6bb3c693a70e0cae6ff9ea94d682d820827cbfa
Thiago da Silva Gomes <thiagosilvagomes@gmail.com>
2021-03-02 19:17:08 -0300

    teste

--- bloco_11/11_2/exercises/introducao-a-react.2/my-pokedex/src/pokemonList.js
+++ bloco_11/11_2/exercises/introducao-a-react.2/my-pokedex/src/pokemonList.js
@@ -2,13 +2,14 @@ import React from 'react';
 import Pokemon from './pokemon'
 
 class PokemonList extends React.Component {
-    render () {
-        return (
-            <div>
-                {this.props.data.map((poke, index) => <Pokemon pokeInfo={poke} key={index}/>)}
-            </div>
-        )
-    }
+  render () {
+    const { data } = this.props;
+    return (
+      <div>
+        {data.map((poke, index) => <Pokemon pokeInfo={poke} key={index}/>)}
+      </div>
+    )
+  }
 }
 
 export default PokemonList;
